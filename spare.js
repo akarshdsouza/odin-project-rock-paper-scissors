76074b5200996313d5c9536394edfcfb207a563e
Akarsh <akarsh.bangalore@gmail.com>
2023-06-28 17:50:21 -0500

    Improve functionality and make changes to code to map functionality to UI better

--- spare.js
+++ spare.js
@@ -11,21 +11,35 @@ window.addEventListener('DOMContentLoaded', function () {
     rockBtn.addEventListener('click', function choices () {
         let playerChoice = 0;
         let computerChoice = getComputerChoice();
-        playGame(playerChoice, computerChoice);
+        playRound(playerChoice, computerChoice);
     })
     
     paperBtn.addEventListener('click', function choices () {
         let playerChoice = 1;
         let computerChoice = getComputerChoice();
-        playGame(playerChoice, computerChoice);
+        playRound(playerChoice, computerChoice);
     })
     
     scissorsBtn.addEventListener('click', function choices () {
         let playerChoice = 2;
         let computerChoice = getComputerChoice();
-        playGame(playerChoice, computerChoice);
+        playRound(playerChoice, computerChoice);
     })
+
+    restartBtn.disabled = true;
+
+    restartBtn.addEventListener('click', function restartGame() {
+        playerWin = 0;
+        compWin = 0;
+        round = 0;
+        restartBtn.disabled = true;
+        roundResult.textContent = "The first player to reach 5 points, wins!";
+    });
 })
+
+let playerWin = 0;
+let compWin = 0;
+let round = 0;
 
 function getComputerChoice () {
     let computerChoice = [0, 1, 2];
@@ -49,17 +63,15 @@ function nameChoices (choice) {
     }
 }
 
-function playGame (player, computer) {
-    let playerWin = 0;
-    let compWin = 0;
-    let round = 0;
+function playRound (player, computer) {
     let playerChoice = nameChoices(player);
     let computerChoice = nameChoices(computer);
-
+    if (playerWin === 5 || compWin === 5) {
+        endGame();
+    }
     if (player === computer) {
         round++;
         roundResult.textContent = (`Both players have played ${playerChoice}! It is a tie!`);
-        return;
     }
     else if ((player === 0 && computer === 1)
     || (player === 1 && computer === 2)
@@ -75,4 +87,19 @@ function playGame (player, computer) {
         playerWin++;
         roundResult.textContent = (`${playerChoice} beats ${computerChoice}! You win!`);
     }
+}
+
+function endGame() {
+    alert(`${round} rounds played and...`)
+    if (playerWin === 5 || compWin === 5) {
+        if(playerWin > compWin) {
+            alert("The game has ended. You WON!!")
+        }
+        else {
+            alert("The game has ended. You lost! :(")
+        }
+    }
+    alert("Please click the restart button on the page to restart a new game.")
+    restartBtn.disabled = false;
+    return;
 }
